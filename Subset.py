398da008280ff1e3334f226d703883d288205668
notmanav <manav.aws@gmail.com>
2017-05-13 22:49:33 -0700

    Adding Subset.py back

---
+++ Subset.py
@@ -0,0 +1,37 @@
+findtext=raw_input("Enter Letters\n")
+#findtext=""
+#findtext="carrot"
+f=open("./res/words.txt")
+#f=open("./res/words-test.txt")
+nextline=f.readline().rstrip()
+loopcontrol=0
+minletters=3
+maxletters=6
+wordcp=""
+res=list()
+while nextline!="" and loopcontrol<100000000:
+    #print(len(nextline))
+    if len(nextline)<=maxletters:
+        wordcp=str(nextline)
+        foundoneletter=0
+        for i in xrange(0,len(findtext)):
+            if findtext[i] in nextline:
+                nextline=nextline.replace(findtext[i],'',1)
+                foundoneletter+=1
+        if foundoneletter>=minletters and foundoneletter==len(wordcp):
+            res.append(wordcp)
+            #print(wordcp)
+    nextline=f.readline().rstrip()
+    loopcontrol+=1
+res.sort(key=len)
+for i in xrange(len(res)):
+    print(res[i]),
+    if i%3==0:
+        print
+#print ('[%s]' % ', '.join(map(str, res)))
+#print(res)
+#print(loopcontrol)
+    
+    
+    
+    
